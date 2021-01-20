8990c04de696a50ef39a03449fd32b44b08de26d
James Gardiner <james@jgardiner.co.uk>
2021-01-20 13:42:33 +0000

    Fail deploy script early if env vars not set

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [[ -z "$NOTIFY_API_KEY" ]]; then
+    echo "NOTIFY_API_KEY must be provided" 1>&2
+    exit 1
+fi
+if [[ -z "$NOTIFY_TEST_TEMPLATE" ]]; then
+    echo "NOTIFY_TEST_TEMPLATE must be provided" 1>&2
+    exit 1
+fi
 export $(cat .env | xargs) &&
 gcloud functions deploy eq-submission-confirmation-consumer \
     --entry-point notify --runtime python39 --trigger-http \
