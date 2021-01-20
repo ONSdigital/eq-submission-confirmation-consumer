fd171e77efa8e9f598c48beba44377097a63f033
James Gardiner <james@jgardiner.co.uk>
2021-01-20 14:23:33 +0000

    Swap script execution order

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export $(cat .env | xargs) &&
 if [[ -z "$NOTIFY_API_KEY" ]]; then
     echo "NOTIFY_API_KEY must be provided" 1>&2
     exit 1
@@ -7,7 +8,6 @@ if [[ -z "$NOTIFY_TEST_TEMPLATE_ID" ]]; then
     echo "NOTIFY_TEST_TEMPLATE must be provided" 1>&2
     exit 1
 fi
-export $(cat .env | xargs) &&
 gcloud functions deploy eq-submission-confirmation-consumer \
     --entry-point notify --runtime python39 --trigger-http \
     --set-env-vars NOTIFY_TEST_TEMPLATE_ID=${NOTIFY_TEST_TEMPLATE_ID},NOTIFY_API_KEY=${NOTIFY_API_KEY} \
