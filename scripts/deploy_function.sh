a6df80838c378a75a5447be9be378d7a4fee1423
Rhys Berrow <rhys.berrow@ons.gov.uk>
2024-02-29 16:53:23 +0000

    Update dependencies

--- scripts/deploy_function.sh
+++ scripts/deploy_function.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcloud functions deploy eq-submission-confirmation-consumer \
-    --entry-point send_email --runtime python38 --trigger-http \
+    --entry-point send_email --runtime python311 --trigger-http \
     --region=europe-west2 -q
