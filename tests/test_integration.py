aa17e39ff60e0e2cd3e3e5a8cd53304ce9f6e763
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2024-05-31 11:22:42 +0100

    update intergration tests to use correct email for payload
    Signed-off-by: Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,10 +3,10 @@ import pytest
 
 @pytest.mark.usefixtures("notify_function_process")
 class TestNotify:
-    payload = {
+    payload_template = {
         "payload": {
             "fulfilmentRequest": {
-                "email_address": "simulate-delivered@notifications.service.gov.uk",
+                "email_address": "",
                 "display_address": "My House, at the end of my street",
                 "form_type": "H",
                 "language_code": "en",
@@ -15,8 +15,14 @@ class TestNotify:
         }
     }
 
+    def set_email(self, email):
+        payload_copy = self.payload_template.copy()
+        payload_copy["payload"]["fulfilmentRequest"]["email_address"] = email
+        return payload_copy
+
     def test_successful(self, base_url, requests_session):
-        res = requests_session.post(base_url, json=self.payload)
+        payload = self.set_email("simulate-delivered@notifications.service.gov.uk")
+        res = requests_session.post(base_url, json=payload)
         assert res.status_code == 201
         assert res.text == "notify request successful"
 
@@ -25,10 +31,12 @@ class TestNotify:
     # (see https://docs.notifications.service.gov.uk/rest-api.html#test)
     @pytest.mark.xfail
     def test_temporary_error(self, base_url, requests_session):
-        res = requests_session.post(base_url, json=self.payload)
+        payload = self.set_email("temp-fail@simulator.notify")
+        res = requests_session.post(base_url, json=payload)
         assert res.status_code != 200
 
     @pytest.mark.xfail
     def test_permanent_error(self, base_url, requests_session):
-        res = requests_session.post(base_url, json=self.payload)
+        payload = self.set_email("perm-fail@simulator.notify")
+        res = requests_session.post(base_url, json=payload)
         assert res.status_code != 200
