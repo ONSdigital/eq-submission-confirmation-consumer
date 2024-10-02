57128ceadbfaf952e68d500223f74bf09ebb1785
Liam Toozer <liam.toozer@gmail.com>
2024-10-02 16:02:33 +0100

    Update project name with var

--- ci/deploy_credentials.sh
+++ ci/deploy_credentials.sh
@@ -12,10 +12,11 @@ if [[ -z "$NOTIFY_API_KEY_FILE" ]]; then
 fi
 
 KEY_NAME="notify_api_key"
+PROJECT_NUMBER=$(gcloud projects describe "${PROJECT_ID}" --format="value(projectNumber)")
 
 gcloud secrets create "$KEY_NAME" --replication-policy="automatic" --project="$PROJECT_ID" || true
 gcloud secrets versions add "$KEY_NAME" --data-file="$NOTIFY_API_KEY_FILE" --project="$PROJECT_ID"
 
-# Give the default App Engine service account access to this secret
+# Give the default compute service account access to this secret
 gcloud secrets add-iam-policy-binding "$KEY_NAME" --role roles/secretmanager.secretAccessor \
-  --member "serviceAccount:$PROJECT_ID-compute@developer.gserviceaccount.com" --project="$PROJECT_ID"
+  --member "serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" --project="$PROJECT_ID"
