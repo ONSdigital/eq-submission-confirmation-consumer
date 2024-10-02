de73ebe828fcefddef793ec2310e2db22d505585
Liam Toozer <liam.toozer@gmail.com>
2024-10-02 12:35:40 +0100

    Bump function deployment runtime to Python 3.12

--- scripts/deploy_function.sh
+++ scripts/deploy_function.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcloud functions deploy eq-submission-confirmation-consumer \
-    --entry-point send_email --runtime python311 --trigger-http \
+    --entry-point send_email --runtime python312 --trigger-http \
     --region=europe-west2 -q
