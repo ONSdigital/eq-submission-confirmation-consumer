6ba25a4b18a6bd38e401e078be6b9bed5b1ac018
Farhanam76 <farhanna700@gmail.com>
2025-11-17 12:34:24 +0000

    Update runtime to 3.13

--- scripts/deploy_function.sh
+++ scripts/deploy_function.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcloud functions deploy eq-submission-confirmation-consumer \
-    --entry-point send_email --runtime python312 --trigger-http \
+    --entry-point send_email --runtime python313 --trigger-http \
     --region=europe-west2 -q
